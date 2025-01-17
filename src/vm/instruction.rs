279ccf90a847c424e13a4459ac578cee0968151c
lilpipidron <lociX55lim@gmail.com>
2025-01-17 04:19:50 +0300

    feat: add instructions

---
+++ src/vm/instruction.rs
@@ -0,0 +1,54 @@
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Instruction {
+    PushInt(i32),
+    PushFloat(f32),
+    PushStr(String),
+    PushChar(char),
+
+    Load(String),  //val to stack
+    Store(String), //val from stack
+
+    AddInt,
+    SubInt,
+    MulInt,
+    DivInt,
+    Mod,
+
+    AddFloat,
+    SubFloat,
+    MulFloat,
+    DivFloat,
+
+    Equal,
+    NotEqual,
+
+    LessInt,
+    LessEqualInt,
+    GreaterInt,
+    GreaterEqualInt,
+
+    LessFloat,
+    LessEqualFloat,
+    GreaterFloat,
+    GreaterEqualFloat,
+
+    Concat,
+
+    Jump(usize),
+    JumpIfTrue(usize),
+    JumpIfFalse(usize),
+
+    Call(string), //func call
+    Return,
+
+    NewStruct(String),
+    SetField(String),
+    GetField(String),
+
+    Print,
+    Println,
+
+    Halt, //end program ??
+    Nop,  //nothing ??
+    STW,  //stop the world for hot swap ??
+}
