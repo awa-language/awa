e25141c17c4601bd5dd4540bb1cb68bba70a3b1f
zzucch <nikitamilovatskiy@gmail.com>
2025-01-23 00:54:09 +0300

    feat: add basic user interaction

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -1,6 +1,8 @@
+use std::sync::mpsc::{channel, Receiver, Sender};
+
 use camino::Utf8PathBuf;
 
-use crate::interpreter;
+use crate::interpreter::{self, Command};
 
 pub fn handle(filename: Option<Utf8PathBuf>) {
     let filename = match filename {
@@ -8,12 +10,25 @@ pub fn handle(filename: Option<Utf8PathBuf>) {
         None => "main.awa".into(),
     };
 
-    let input = std::fs::read_to_string(filename);
-    let input = match input {
+    let _input = std::fs::read_to_string(filename);
+    let _input = match _input {
         Ok(input) => input,
         Err(_err) => todo!(),
     };
 
-    let () = interpreter::build_ast(&input);
-    interpreter::run(); // TODO: will take typed ast module as an argument
+    // let () = interpreter::build_ast(&_input);
+
+    let (input_sender, input_reciever): (Sender<Command>, Receiver<Command>) = channel();
+    let (confirmation_sender, confirmation_reciever): (Sender<()>, Receiver<()>) = channel();
+
+    let _ = std::thread::spawn(move || {
+        interpreter::run(input_reciever, confirmation_sender);
+    });
+
+    let term = console::Term::stdout();
+    loop {
+        let _ = term.read_char().unwrap();
+        let _ = input_sender.send(Command::OpenMenu).unwrap();
+        let _ = confirmation_reciever.recv();
+    }
 }
--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,10 +1,33 @@
-use crate::parse;
+use crate::{cli, parse};
 
-pub fn run() {
-    todo!()
+#[derive(Debug)]
+pub enum Command {
+    OpenMenu,
+}
+
+// TODO: will take typed ast module as an argument
+pub fn run(receiver: std::sync::mpsc::Receiver<Command>, sender: std::sync::mpsc::Sender<()>) {
+    // TODO: vm::new()
+
+    loop {
+        dbg!("looping");
+        if let Ok(command) = receiver.try_recv() {
+            dbg!(command);
+            match command {
+                Command::OpenMenu => {
+                    let user_input = cli::input::get_user_input();
+                    dbg!(user_input);
+                    let _ = sender.send(()).unwrap();
+                }
+            }
+        }
+        // TODO: vm::run()
+    }
 }
 
 pub fn build_ast(input: &str) {
     let _module = parse::parse_module(input);
+
+    // TODO: TAST+translation
     todo!();
 }
