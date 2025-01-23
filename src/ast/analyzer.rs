6971b2b5bccf508e82bdde860753bc31c5e80740
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2025-01-24 02:25:11 +0300

    fix: append support

--- src/ast/analyzer.rs
+++ src/ast/analyzer.rs
@@ -1193,12 +1193,32 @@ impl ProgramState {
     }
 
     fn get_function(&self, name: &EcoString) -> Option<DefinitionTyped> {
-        if name == "print" || name == "println" || name == "append" {
+        if name == "print" || name == "println" {
             let function = DefinitionTyped::Function {
                 name: name.clone(),
                 location: ast::location::Location { start: 0, end: 0 },
                 arguments: Some(Vec1::try_from(vec![
                     ArgumentTyped {
+                        name: Default::default(),
+                        location: ast::location::Location { start: 0, end: 0 },
+                        type_: Type::Int,
+                    }
+                ]).unwrap()),
+                body: None,
+                return_type: Type::Void,
+            };
+            return Some(function);
+        }
+        if name == "append" {
+            let function = DefinitionTyped::Function {
+                name: name.clone(),
+                location: ast::location::Location { start: 0, end: 0 },
+                arguments: Some(Vec1::try_from(vec![
+                    ArgumentTyped {
+                        name: Default::default(),
+                        location: ast::location::Location { start: 0, end: 0 },
+                        type_: Type::Int,
+                    },  ArgumentTyped {
                         name: Default::default(),
                         location: ast::location::Location { start: 0, end: 0 },
                         type_: Type::Int,
